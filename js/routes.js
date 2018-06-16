dc1adb973c484b39a4a8b676f55dbdf139b7679e
Caique M Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-16 18:43:00 -0300

    parse do json retornado - Gastos

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,5 @@
 const host = "127.0.0.1";
 
 const route = {
-	"gatos_pessoais":"app-sisgvconsulta-prd.azurewebsites.net/ws/ws2.asmx/ObterDebitoVereadorJSON"
+	"gastos_pessoais":"http://app-sisgvconsulta-prd.azurewebsites.net/ws/ws2.asmx/ObterDebitoVereadorJSON"
 };
