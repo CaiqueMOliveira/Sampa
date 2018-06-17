0bff9c5b2c1b29e1e4c51898fc27b91349927b62
Matheus Alves <mat.alves.souza@outlook.com>
2018-06-17 09:04:17 -0300

    Chat

--- www/js/utils.js
+++ www/js/utils.js
@@ -1,6 +1,11 @@
 const answersChat = [
-	"Acheio o gasto mesmo assim razoável :|",
+	"Achei o gasto razoável mesmo assim  :|",
 	"Ainda está alto. Vamos abaixar isto aí...",
 	"Agora sim poderemos realmente saber para que é utilizado o nosso dinheiro",
-	"Interessante as análises do app =)"
+	"Interessante as análises do app =)",
+	"Hahaha , vou compartilhar isso ai !",
+	"Será mesmo que ele tem a noção disso ?",
+	"Todos devem saber disso",
+	"Um pouco contestavel",
+	"Creio que toda verba pública devia ser repensada"
 ];
