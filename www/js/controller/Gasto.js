314ff758216d8a6c8e86f34d493d399fa2728ed8
Matheus Alves <mat.alves.souza@outlook.com>
2018-06-17 00:45:50 -0300

    Apis

--- www/js/controller/Gasto.js
+++ www/js/controller/Gasto.js
@@ -4,8 +4,24 @@ class Gasto
 	/**
 	 * Obtem os gastos publicos por agrupados pelo vereador
 	 */
-	static getGastosPessoais(callbacksuccess, callbackfail)
+	static getGastosPessoais(mes, ano, callbacksuccess, callbackfail)
 	{
 		new GastoDAO().getGastosPessoais(callbacksuccess, callbackfail); 
+	}
+
+	/**
+	 * Obtem os gastos publicos por agrupados pelo vereador e por tipo de despesa
+	 */
+	static getGastosEspecificosPessoais(chave,callbacksuccess, callbackfail)
+	{
+		new GastoDAO().getGastosEspecificosPessoais(chave,callbacksuccess, callbackfail); 
+	}
+
+	/**
+	 * Obtem todos os gastos (de todos) de um determinado mes e ano
+	 */
+	static getGastosTotais(mes, ano , callbacksuccess, callbackfail)
+	{
+		new GastoDAO().getGastosTotais(mes, ano , callbacksuccess, callbackfail); 
 	}
 }
